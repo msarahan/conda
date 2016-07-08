ae3c79fdff85b2745a0a0b488d5c6e82e49e0f41
Dylan Orzel <dylan.orzel@gmail.com>
2016-07-08 11:10:12 -0500

    move all custom exceptions to exceptions.py, fix some tests

--- tests/test_resolve.py
+++ tests/test_resolve.py
@@ -5,8 +5,9 @@ from os.path import dirname, join
 
 import pytest
 
-from conda.resolve import MatchSpec, Package, Resolve, NoPackagesFound, Unsatisfiable
-from helpers import raises
+from conda.resolve import MatchSpec, Package, Resolve
+from conda.exceptions import NoPackagesFoundError, UnsatisfiableError
+from tests.helpers import raises
 from conda import config
 
 with open(join(dirname(__file__), 'index.json')) as fi:
@@ -97,6 +98,7 @@ class TestMatchSpec(unittest.TestCase):
     def test_string(self):
         a = MatchSpec("foo1 >=1.3 2 (optional,target=burg)")
         assert a.optional and a.target=='burg'
+
 
 class TestPackage(unittest.TestCase):
 
@@ -361,16 +363,16 @@ def test_generate_eq():
 @pytest.mark.benchmark
 def test_unsat():
     # scipy 0.12.0b1 is not built for numpy 1.5, only 1.6 and 1.7
-    assert raises(Unsatisfiable, lambda: r.install(['numpy 1.5*', 'scipy 0.12.0b1']))
+    assert raises(UnsatisfiableError, lambda: r.install(['numpy 1.5*', 'scipy 0.12.0b1']))
     # numpy 1.5 does not have a python 3 package
-    assert raises(Unsatisfiable, lambda: r.install(['numpy 1.5*', 'python 3*']))
-    assert raises(Unsatisfiable, lambda: r.install(['numpy 1.5*', 'numpy 1.6*']))
+    assert raises(UnsatisfiableError, lambda: r.install(['numpy 1.5*', 'python 3*']))
+    assert raises(UnsatisfiableError, lambda: r.install(['numpy 1.5*', 'numpy 1.6*']))
 
 def test_nonexistent():
-    assert raises(NoPackagesFound, lambda: r.get_pkgs('notarealpackage 2.0*'))
-    assert raises(NoPackagesFound, lambda: r.install(['notarealpackage 2.0*']))
+    assert raises(NoPackagesFoundError, lambda: r.get_pkgs('notarealpackage 2.0*'))
+    assert raises(NoPackagesFoundError, lambda: r.install(['notarealpackage 2.0*']))
     # This exact version of NumPy does not exist
-    assert raises(NoPackagesFound, lambda: r.install(['numpy 1.5']))
+    assert raises(NoPackagesFoundError, lambda: r.install(['numpy 1.5']))
 
 @pytest.mark.benchmark
 def test_nonexistent_deps():
@@ -444,8 +446,8 @@ def test_nonexistent_deps():
         'tk-8.5.13-0.tar.bz2',
         'zlib-1.2.7-0.tar.bz2',
     ]
-    assert raises(NoPackagesFound, lambda: r.install(['mypackage 1.0']))
-    assert raises(NoPackagesFound, lambda: r.install(['mypackage 1.0', 'burgertime 1.0']))
+    assert raises(NoPackagesFoundError, lambda: r.install(['mypackage 1.0']))
+    assert raises(NoPackagesFoundError, lambda: r.install(['mypackage 1.0', 'burgertime 1.0']))
 
     assert r.install(['anotherpackage 1.0']) == [
         'anotherpackage-1.0-py33_0.tar.bz2',
@@ -544,7 +546,7 @@ def test_nonexistent_deps():
         'tk-8.5.13-0.tar.bz2',
         'zlib-1.2.7-0.tar.bz2',
     ]
-    assert raises(NoPackagesFound, lambda: r.install(['mypackage 1.1']))
+    assert raises(NoPackagesFoundError, lambda: r.install(['mypackage 1.1']))
 
 
     assert r.install(['anotherpackage 1.0']) == [
