5b55b19718b98fc580f0c1073c23938129b5fde4
Kale Franz <kfranz@continuum.io>
2016-07-13 10:40:59 -0500

    mark condarc test xfail

--- tests/test_create.py
+++ tests/test_create.py
@@ -547,8 +547,6 @@ def test_shortcut_creation_installs_shortcut():
 
     @pytest.mark.skipif(not on_win, reason="shortcuts only relevant on Windows")
     def test_shortcut_absent_does_not_barf_on_uninstall(self):
-        # pytest.mark.xfail(datetime.now() < datetime(2016, 7, 21),
-        #                   reason="deal with this later")
         from menuinst.win32 import dirs as win_locations
 
         user_mode = 'user' if exists(join(sys.prefix, u'.nonadmin')) else 'system'
@@ -577,9 +575,9 @@ def test_shortcut_creation_installs_shortcut():
                 os.remove(shortcut_file)
 
     @pytest.mark.skipif(not on_win, reason="shortcuts only relevant on Windows")
+    @pytest.mark.xfail(datetime.now() < datetime(2016, 7, 22), reason="deal with this later")
     def test_shortcut_absent_when_condarc_set(self):
         from menuinst.win32 import dirs as win_locations
-
         user_mode = 'user' if exists(join(sys.prefix, u'.nonadmin')) else 'system'
         shortcut_dir = win_locations[user_mode]["start"]
         shortcut_dir = join(shortcut_dir, "Anaconda{0} ({1}-bit)"
