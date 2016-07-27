d100a7c2056ace8b3ca18005e4a3c16e3b4ffe74
Zhang Tian <zhangtian1000@gmail.com>
2016-07-26 22:44:26 -0500

    flake8

--- conda/utils.py
+++ conda/utils.py
@@ -11,7 +11,8 @@ import tempfile
 import threading
 from functools import partial
 from os.path import isdir, join, basename, exists
-
+# conda build import
+from .common.url import path_to_url
 log = logging.getLogger(__name__)
 stderrlog = logging.getLogger('stderrlog')
 
@@ -416,5 +417,4 @@ def exp_backoff_fn(fn, *args):
             return result
 
 # put back because of conda build
-from .common.url import path_to_url
 urlpath = path_to_url
