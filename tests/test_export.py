fbd7d758ae9851cf886887525d832d127c7a2b7f
tester <zhangtian1000@gmail.com>
2016-08-18 09:02:27 -0500

    merge with origin

--- tests/test_export.py
+++ tests/test_export.py
@@ -17,6 +17,34 @@ class ExportIntegrationTests(TestCase):
             assert_package_is_installed(prefix, 'python-3')
 
             output, error = run_command(Commands.LIST, prefix, "-e")
+
+            with tempfile.NamedTemporaryFile(mode="w", suffix="txt", delete=False) as env_txt:
+                env_txt.write(output)
+                env_txt.flush()
+                env_txt.close()
+                prefix2 = make_temp_prefix()
+                run_command(Commands.CREATE, prefix2 , "--file " + env_txt.name)
+
+                assert_package_is_installed(prefix2, "python")
+
+            output2, error= run_command(Commands.LIST, prefix2, "-e")
+            self.assertEqual(output, output2)
+
+    def test_multi_channel_export(self):
+        """
+            When try to import from txt
+            every package should come from same channel
+        """
+        with make_temp_env("python=3") as prefix:
+            assert exists(join(prefix, PYTHON_BINARY))
+            assert_package_is_installed(prefix, 'python-3')
+
+            run_command(Commands.INSTALL, prefix, "six", "-c", "conda-forge")
+            assert_package_is_installed(prefix, "six")
+
+            output, error = run_command(Commands.LIST, prefix, "-e")
+            self.assertIn("conda-forge", output)
+            
             try:
                 with tempfile.NamedTemporaryFile(mode="w", suffix="txt", delete=False) as env_txt:
                     env_txt.write(output)
@@ -29,6 +57,7 @@ class ExportIntegrationTests(TestCase):
                 self.assertEqual(output, output2)
             finally:
                 rm_rf(env_txt.name)
+
 
     def test_multi_channel_explicit(self):
         """
@@ -58,3 +87,4 @@ class ExportIntegrationTests(TestCase):
                 self.assertEqual(output, output2)
             finally:
                 rm_rf(env_txt.name)
+
