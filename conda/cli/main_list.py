d060e12b61f75058aa7770a17f5e4b8248c779ae
Kale Franz <kfranz@continuum.io>
2016-09-06 08:04:15 -0500

    quiet, update_deps, offline, show_channel_urls

--- conda/cli/main_list.py
+++ conda/cli/main_list.py
@@ -225,5 +225,5 @@ def execute(args, parser):
 
     exitcode = print_packages(prefix, regex, format, piplist=args.pip,
                               json=args.json,
-                              show_channel_urls=args.show_channel_urls)
+                              show_channel_urls=context.show_channel_urls)
     return exitcode
