551d29428a150ccefa9f22b1eeb3f5660cf8fb16
Kale Franz <kfranz@continuum.io>
2016-11-29 10:26:32 -0600

    Revert "Merge pull request #3175 from njalerikson/master"
    This reverts commit 4fd10ddc34a48a6cd41ef8f3c3127011e4e24a65, reversing
    changes made to 31097f31f3f202ce6a50b55a9424131bbcce641c.

--- conda/install.py
+++ conda/install.py
@@ -24,39 +24,9 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import functools
 import logging
 import os
-import re
-import shlex
-import shutil
-import stat
-import struct
-import subprocess
-import sys
-import tarfile
-import traceback
-from os.path import abspath, basename, dirname, isdir, isfile, islink, join
-
-try:
-    from conda.lock import Locked
-    from conda.utils import win_path_to_unix, url_path
-    from conda.config import remove_binstar_tokens, pkgs_dirs, url_channel
-    import conda.config as config
-except ImportError:
-    # Make sure this still works as a standalone script for the Anaconda
-    # installer.
-    pkgs_dirs = [sys.prefix]
-
-    class Locked(object):
-        def __init__(self, *args, **kwargs):
-            pass
-
-        def __enter__(self):
-            pass
-
-        def __exit__(self, exc_type, exc_value, traceback):
-            pass
-
-    def win_path_to_unix(path, root_prefix=""):
-        """Convert a path or ;-separated string of paths into a unix representation
+from errno import EACCES, EEXIST, EPERM, EROFS
+from os import chmod, makedirs, stat
+from os.path import dirname, isdir, isfile, join, normcase, normpath
 
 from .base.constants import PREFIX_PLACEHOLDER
 from .gateways.disk.delete import delete_trash, move_path_to_trash, rm_rf
@@ -86,7 +56,7 @@ if on_win:
 
         Works of course only with callable files, e.g. `.bat` or `.exe` files.
         """
-        # ensure that directory exists first
+        from conda.utils import shells
         try:
             makedirs(dirname(dst))
         except OSError as exc:  # Python >2.5
@@ -98,67 +68,23 @@ if on_win:
         # bat file redirect
         if not isfile(dst + '.bat'):
             with open(dst + '.bat', 'w') as f:
-                f.write(dedent("""\
-                    @echo off
-                    call "{}" %%*
-                    """).format(src))
+                f.write('@echo off\ncall "%s" %%*\n' % src)
 
         # TODO: probably need one here for powershell at some point
 
-    def win_conda_unix_redirect(src, dst, shell):
-        """Special function for Windows where the os.symlink function
-        is unavailable due to a lack of user priviledges.
-
-        Simply creates a source-able intermediate file.
-        """
-        # ensure that directory exists first
-        try:
-            os.makedirs(os.path.dirname(dst))
-        except OSError as exc:  # Python >2.5
-            if exc.errno == EEXIST and os.path.isdir(os.path.dirname(dst)):
-                pass
-            else:
-                raise
+        # This one is for bash/cygwin/msys
+        # set default shell to bash.exe when not provided, as that's most common
+        if not shell:
+            shell = "bash.exe"
 
         # technically these are "links" - but islink doesn't work on win
         if not isfile(dst):
             with open(dst, "w") as f:
-                shell_vars = shells[shell]
-
-                # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-                # !! ensure the file ends with a blank line this is       !!
-                # !! critical for Windows support                         !!
-                # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-                # conda is used as an executable
+                f.write("#!/usr/bin/env bash \n")
                 if src.endswith("conda"):
-                    command = shell_vars['path_to'](src+".exe")
-                    text = dedent("""\
-                        #!/usr/bin/env {shebang}
-                        {command} {allargs}
-                        """).format(
-                        shebang=re.sub(
-                            r'\.\w+$',
-                            r'',
-                            os.path.basename(shell_vars["exe"])),
-                        command=command,
-                        **shell_vars)
-                    f.write(text)
-                # all others are used as sourced
+                    f.write('%s "$@"' % shells[shell]['path_to'](src+".exe"))
                 else:
-                    command = shell_vars["source"].format(shell_vars['path_to'](src))
-                    text = dedent("""\
-                        #!/usr/bin/env {shebang}
-                        {command} {allargs}
-                        """).format(
-                        shebang=re.sub(
-                            r'\.\w+$',
-                            r'',
-                            os.path.basename(shell_vars["exe"])),
-                        command=command,
-                        **shell_vars)
-                    f.write(text)
-
+                    f.write('source %s "$@"' % shells[shell]['path_to'](src))
             # Make the new file executable
             # http://stackoverflow.com/a/30463972/1170370
             mode = stat(dst).st_mode
@@ -567,23 +493,12 @@ def symlink_conda(prefix, root_dir, shell=None):
     # prefix should always be longer than, or outside the root dir.
     if normcase(normpath(prefix)) in normcase(normpath(root_dir)):
         return
-
-    if shell is None:
-        shell = "bash.msys"
-
     if on_win:
         where = 'Scripts'
+        symlink_fn = functools.partial(win_conda_bat_redirect, shell=shell)
     else:
         where = 'bin'
-
-    if on_win:
-        if shell in ["cmd.exe", "powershell.exe"]:
-            symlink_fn = functools.partial(win_conda_bat_redirect, shell=shell)
-        else:
-            symlink_fn = functools.partial(win_conda_unix_redirect, shell=shell)
-    else:
         symlink_fn = os.symlink
-
     if not isdir(join(prefix, where)):
         os.makedirs(join(prefix, where))
     symlink_conda_hlp(prefix, root_dir, where, symlink_fn)
@@ -601,8 +516,7 @@ def symlink_conda_hlp(prefix, root_dir, where, symlink_fn):
             # try to kill stale links if they exist
             if os.path.lexists(prefix_file):
                 rm_rf(prefix_file)
-
-            # if they're in use, they won't be killed, skip making new symlink
+            # if they're in use, they won't be killed.  Skip making new symlink.
             if not os.path.lexists(prefix_file):
                 symlink_fn(root_file, prefix_file)
         except (IOError, OSError) as e:
