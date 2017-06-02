8467a157f00e1fe1bd0f58bb86e40fab37e5d589
Kale Franz <kfranz@continuum.io>
2017-06-02 16:06:12 -0500

    more coverage and cleanup

--- tests/test_create.py
+++ tests/test_create.py
@@ -1250,6 +1250,8 @@ class IntegrationTests(TestCase):
 
         assert all(isdir(d) for d in itervalues(cache_dirs))
 
+        run_command(Commands.CLEAN, '', "--source-cache --yes")
+
         # --json flag is regression test for #5451
         run_command(Commands.CLEAN, '', "--source-cache --yes  --json")
 
