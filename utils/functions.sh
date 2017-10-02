fd8272edc3b534fdbd2fe0da9138717eaa9033b0
Kale Franz <kfranz@continuum.io>
2017-10-02 15:36:11 -0500

    fix conda-build setup caused by merge error

--- utils/functions.sh
+++ utils/functions.sh
@@ -359,22 +359,7 @@ conda_build_test() {
     conda info
     echo "safety_checks: disabled" >> ~/.condarc
 
-    $prefix/bin/python -m pytest --basetemp /tmp/cb -v --durations=20 -n 0 -m "serial" tests -k "not xattr and not skeleton_pypi"
-    $prefix/bin/python -m pytest --basetemp /tmp/cb -v --durations=20 -n 2 -m "not serial" tests -k "not xattr and not skeleton_pypi"
-    popd
-}
-
-
-osx_setup() {
-    # brew update || brew update
-    # brew outdated openssl || brew upgrade openssl
-    brew install zsh
-
-    # rvm get head
-
-    install_conda_dev
-}
-
+    pushd conda-build
 
     # TODO: remove -k flag when conda/conda-build#1927 is merged
     $prefix/$BIN_DIR/python -m pytest --basetemp /tmp/cb -v --durations=20 -n 2 -m "not serial" tests \
