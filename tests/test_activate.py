e5ee4b16b99071fa299e609f1da51d50608d3567
Kale Franz <kfranz@continuum.io>
2018-02-22 16:43:18 -0600

    also add removal of CONDA_EXE on last deactivate

--- tests/test_activate.py
+++ tests/test_activate.py
@@ -377,6 +377,7 @@ def gen_test_env_paths(envs, shell, num_test_folders=5):
                         'CONDA_PREFIX',
                         'CONDA_DEFAULT_ENV',
                         'CONDA_PYTHON_EXE',
+                        'CONDA_EXE',
                         'CONDA_PROMPT_MODIFIER',
                     )
 
@@ -729,6 +730,7 @@ def test_activate_help(shell):
             assert deactivate_data == dals("""
             \\. "%(deactivate1)s"
             \\unset CONDA_DEFAULT_ENV
+            \\unset CONDA_EXE
             \\unset CONDA_PREFIX
             \\unset CONDA_PROMPT_MODIFIER
             \\unset CONDA_PYTHON_EXE
@@ -896,6 +898,7 @@ def test_activate_help(shell):
             assert deactivate_data == dals("""
             source "%(deactivate1)s";
             unset CONDA_DEFAULT_ENV;
+            unset CONDA_EXE;
             unset CONDA_PREFIX;
             unset CONDA_PROMPT_MODIFIER;
             unset CONDA_PYTHON_EXE;
@@ -983,6 +986,7 @@ def test_activate_help(shell):
             assert deactivate_data == dals("""
             source "%(deactivate1)s"
             del $CONDA_DEFAULT_ENV
+            del $CONDA_EXE
             del $CONDA_PREFIX
             del $CONDA_PROMPT_MODIFIER
             del $CONDA_PYTHON_EXE
@@ -1058,6 +1062,7 @@ def test_activate_help(shell):
             assert deactivate_data == dals("""
             source "%(deactivate1)s";
             set -e CONDA_DEFAULT_ENV;
+            set -e CONDA_EXE;
             set -e CONDA_PREFIX;
             set -e CONDA_PROMPT_MODIFIER;
             set -e CONDA_PYTHON_EXE;
@@ -1133,6 +1138,7 @@ def test_activate_help(shell):
             assert deactivate_data == dals("""
             . "%(deactivate1)s"
             Remove-Variable CONDA_DEFAULT_ENV
+            Remove-Variable CONDA_EXE
             Remove-Variable CONDA_PREFIX
             Remove-Variable CONDA_PROMPT_MODIFIER
             Remove-Variable CONDA_PYTHON_EXE
