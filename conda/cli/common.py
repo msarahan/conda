fe25d40f5c649a8b50e42de986b2d3f1a86bdd9a
Kale Franz <kfranz@continuum.io>
2018-05-03 13:20:56 -0500

    json output maintentance
    Signed-off-by: Kale Franz <kfranz@continuum.io>

--- conda/cli/common.py
+++ conda/cli/common.py
@@ -174,7 +174,13 @@ def stdout_json(d):
 
 
 def stdout_json_success(success=True, **kwargs):
+    from ..models.dist import Dist
     result = {'success': success}
+    actions = kwargs.pop('actions')
+    if actions:
+        actions['LINK'] = [Dist(prec) for prec in actions['LINK']]
+        actions['UNLINK'] = [Dist(prec) for prec in actions['UNLINK']]
+        result['actions'] = actions
     result.update(kwargs)
     stdout_json(result)
 
